a45fb7fe46b1b524735a5815cd5b299428ef2a16
Your Name <you@example.com>
2018-04-19 10:54:29 +0200

    First App + ESlint

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static(__dirname + '/assets'))
+.use('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('App listening on port 3000!')
+});
