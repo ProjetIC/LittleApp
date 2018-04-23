9c3dd249cf4d1860f1304ad04945996796df00ca
Unknown <aubin.brosset.pro@gmail.com>
2018-04-23 21:25:48 +0200

    avectestunitMoca

---
+++ test/demoSpec.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+describe('Premier Test', function(){
+	
+	it('faire un truc', function(){
+		var a = 3;
+		assert.equal(a * 2, 6, 'la multiplication n\'a pas fonctionné')
+		
+	})
+})
