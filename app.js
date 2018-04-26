a32b4133c68b4e2ae3551529ccab4618b19776e1
Unknown <aubin.brosset.pro@gmail.com>
2018-04-26 16:37:52 +0200

    eslintOK

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.use(express.static(__dirname + '/assets'))
-.use('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+  .use('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html')
+  })
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
-});
+})
--- test/demoSpec.js
+++ test/demoSpec.js
@@ -1,10 +1,8 @@
-var assert = require('assert');
+var assert = require('assert')
 
-describe('Premier Test', function(){
-	
-	it('faire un truc', function(){
-		var a = 3;
-		assert.equal(a * 2, 6, 'la multiplication n\'a pas fonctionné')
-		
-	})
+describe('Premier Test', function () {
+  it('faire un truc', function () {
+    var a = 3
+    assert.equal(a * 2, 6, 'la multiplication n\'a pas fonctionné')
+  })
 })
