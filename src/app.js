e412358a562d9b85f1fde2fb1c534bedcb0d2798
alex.jadeau <alex.jadeau@outlook.com>
2018-05-15 20:41:44 +0200

    Mongo first release + signin function

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var hbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
 
 var indexRouter = require('./routes/index');
 
@@ -17,8 +20,12 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret: 'user', saveUninitialized: false, resave: false}))
 
 app.use('/', indexRouter);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Little App' });
+});
+
+router.post('/signin', function(req, res, next) {
+  var item = {
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  mongo.connect(url, function(err, client) {
+    var db = client.db('user-data');
+    assert.equal(null, err);
+    db.collection('users').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Data insert!');
+      client.close();
+    });
+  });
+
+  res.redirect('/');
 });
 
 module.exports = router;
