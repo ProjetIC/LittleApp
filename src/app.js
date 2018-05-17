a5e15645559bf402c32d77127efb17ade253f492
alex.jadeau <alex.jadeau@outlook.com>
2018-05-17 21:42:23 +0200

    Reboot new app all works

--- src/app.js
+++ src/app.js
@@ -2,38 +2,44 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var logger = require('morgan');
-var hbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var expressSession = require('express-session');
+var bcrypt = require('bcrypt');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
+// db
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+var db = mongoose.connection;
+
+// routes
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-//DB connection
-var mongoDB = 'mongodb://localhost:27017/test';
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-
-var db = mongoose.connection;
-
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// session
+app.use(session({
+  secret: 'banana',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -50,8 +56,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-//DB connection error
-db.on('error', console.error.bind(console, 'MongoDB error:'));
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../lib/User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Little App' });
+});
+
+router.post('/', function(req, res, next) {
+  if (req.body.email &&
+    req.body.password) {
+
+    var userData = {
+      email: req.body.email,
+      password: req.body.password,
+    }
+    
+    User.create(userData, function(err, user) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/users');
+      }
+    });
+  } else if (req.body.logemail && req.body.logpassword) {
+    User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        return res.redirect('/users');
+      }
+    });
+  }
+});
+
+router.get('/logout', function (req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
 });
 
 module.exports = router;
